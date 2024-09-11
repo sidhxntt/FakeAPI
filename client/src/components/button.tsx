544af9bdd27804fa1851f079f2b14502843af818
Siddhantg2002 <siddhantg2002@gmail.com>
2024-09-12 00:44:22 +0530

    Production Ready

--- client/src/components/button.tsx
+++ client/src/components/button.tsx
@@ -1,17 +1,20 @@
 "use client";
-import Link from "next/link";
+import { useRouter } from "next/navigation"; // Use next/navigation for App Router
 
 const Button = () => {
+  const router = useRouter();
+  
+  const scrollToSection = () => {
+    router.push("#usage"); // Navigate to the section with id 'usage'
+  };
+
   return (
-    <Link 
-    className="z-50"
-    href="#usage">
-     <button
-    className="mt-6 z-50 shadow-[inset_0_0_0_2px_#616467] text-black px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
+    <button
+      onClick={scrollToSection}
+      className="mt-6 z-50 shadow-[inset_0_0_0_2px_#616467] text-black px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200"
+    >
       Get Started
     </button>
-    </Link>
-   
   );
 };
 
