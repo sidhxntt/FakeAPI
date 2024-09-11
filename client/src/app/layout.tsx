a8b328135a41e15d756d3632aaa9651c9f22ebfe
Siddhantg2002 <siddhantg2002@gmail.com>
2024-09-12 00:18:07 +0530

    Production Ready

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Rapid API",
@@ -20,8 +20,8 @@ export default function RootLayout({
           <link rel="icon" href="/logo.png" />
         </head>
       <body className={`antialiased`}>
-      <Navbar/>
         {children}
+        <Footer/>
         </body>
     </html>
   );
--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
-const Navbar = () => {
+const Footer = () => {
   return (
-    <header className="sticky top-0 bg-[#0a0a0a] backdrop-blur-md mb-12 p-4 z-50">
+    <footer className="sticky top-0 bg-[#0a0a0a] backdrop-blur-md mb-12 mt-[-2cm] p-4 z-50">
       <div className="flex items-center justify-end p-4 mx-auto max-w-7xl">
         <div className="flex items-center space-x-6">
           <Link className="text-white" href="https://x.com/sidhxntt" target="_blank" rel="noopener noreferrer">
@@ -21,8 +21,8 @@ const Navbar = () => {
           </Link>
         </div>
       </div>
-    </header>
+    </footer>
   );
 };
 
-export default Navbar;
+export default Footer;
